baf01779bf6f288065a25c281ac630bba1c35a2e
DevJoaoVitorBP <joaov.batalha013@hotmail.com>
2025-12-15 20:50:39 -0300

    Feat: Feat: New page for Season and refactoring the front-end structure.

--- F1DataHub/Pages/Home/Index.cshtml.cs
+++ F1DataHub/Pages/Home/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace F1DataHub.Pages
+namespace F1DataHub.Apresentation.Pages.Home
 {
     public class IndexModel : PageModel
     {
